7776adee02db75dab3e6929d6166b94bf5a26ec5
CharloTTkaa <good_charlotte11@mail.ru>
2023-08-04 14:51:01 +0300

    dz02/06

---
+++ lesson-02/06.js
@@ -0,0 +1,12 @@
+let passportWithAddress = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "LA"
+    }
+};
+
+let passportWithAddress2={...passportWithAddress}
+passportWithAddress2.address.city="Bobryisk"
+console.log(passportWithAddress2)
