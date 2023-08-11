9ac8965812a674038fd14000e0e2b106c69fa92a
CharloTTkaa <good_charlotte11@mail.ru>
2023-08-11 15:31:22 +0300

    03/01

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,9 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+  const flagColors = colors.filter(color => color === "черный" || color === "красный" || color === "желтый");
+  const flagFormula = flagColors.join("-");
+  return flagFormula;
+}
+
+console.log(createColorString()); 
