8a5e268862cd11194bda3ab9e008259ab862a5ce
CharloTTkaa <good_charlotte11@mail.ru>
2023-08-11 22:01:03 +0300

    03/02

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,3 @@
 ​const fruits = ["яблоко", "банан", "апельсин", "манго", "киви", "ананас", "виноград", "груша", "слива", "персик"];
-const newFruits = fruits.slice(0,-2);
-console.log(newFruits);
+const fruits2 = fruits.slice(1,7);
+console.log(fruits2);
