d890612adeb4f6e125bc7d5cb752c130ba5a7cc4
CharloTTkaa <good_charlotte11@mail.ru>
2023-08-15 14:52:04 +0300

    03/07

--- lesson-03/07.js
+++ lesson-03/07.js
@@ -1,11 +1,8 @@
-const numbers = [ 
-    [1, 2, 3, 4, 5], 
-    [10, 20, 30, 40, 50], 
-    [100, 200, 300, 400, 500], 
-]; 
- 
-numbers.map(row => console.log(row)) 
- 
-numbers.map(row => { 
-    row.map(number => console.log(number)); 
-}); 
+const numbers = [
+    [1, 2, 3, 4, 5],
+    [11, 20, 33, 40, 55],
+    [111, 200, 333, 400, 555],
+];
+
+const res = numbers.map(subArray => subArray.filter(num => num % 2 === 0));
+console.log(res);
